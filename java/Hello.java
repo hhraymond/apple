66782bedd608b9e25a008e1d4077061f1b4cf231
hhraymond <skyhuang007@163.com>
2013-05-06 10:10:48 +0800

    add java

---
+++ java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+    public static void main(String args[])
+    {
+        int num = 3;
+        System.out.println("这是数字"+num);
+        System.out.println("Hello World!");
+    }
+}
